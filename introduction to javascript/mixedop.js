04e890e84784726b63e0fdbf58ef24ff994cfd12
mdalaminariya <king0878753@gmail.com>
2024-03-12 20:09:52 +0600

    add mixedop

---
+++ introduction to javascript/mixedop.js
@@ -0,0 +1,12 @@
+var name1 = "Mobarok";
+var name2 = "tobarok";
+
+var fullName = name1 + name2;
+
+// console.log(fullName);
+
+
+console.log(40 +'2');
+
+console.log(40-'10');
+console.log('50'-10);
