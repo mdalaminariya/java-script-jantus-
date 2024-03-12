3ebdba98d41d812ffabfcaf49ee939e54fc5d398
mdalaminariya <king0878753@gmail.com>
2024-03-12 19:46:43 +0600

    add shorted

---
+++ introduction to javascript/shortend.js
@@ -0,0 +1,19 @@
+var price =35;
+// var price = price+15;
+price += 15;
+// console.log(price);
+
+
+var fridge = 35000;
+// var fridge = fridge - 1000;
+
+fridge -= 1000;
+// console.log(fridge);
+
+
+//multiple
+
+var orangePrice = 30;
+// var orangePrice = orangePrice*3
+orangePrice *= 3;
+console.log(orangePrice);
