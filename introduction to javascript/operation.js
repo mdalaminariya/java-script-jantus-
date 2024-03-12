ec9519ce742fd0b7dbf9d20ba95252c3fb9e35d8
mdalaminariya <king0878753@gmail.com>
2024-03-12 19:18:40 +0600

    added some new items

---
+++ introduction to javascript/operation.js
@@ -0,0 +1,30 @@
+var eggPrice = 20;
+var onionPrice = 40;
+
+// console.log(eggPrice);
+// console.log(eggPrice + onionPrice);
+// console.log(eggPrice - onionPrice);
+
+
+// console.log(10/3);
+
+//remainder
+console.log(10%3); //% er maddome vag shesh koto thakbe ta dekhabe..show the remainder dividing by 10/3
+
+// -----------------------------------
+
+
+// man change korar jonne next e ar var bosbe na..directly name pore man bosiye dite hobe.
+
+var money = 550;
+money =450;
+console.log(money);
+
+var price =25;
+price = price+10;
+console.log(price);
+
+var newPrice = price + 50;
+
+var discountPrice = newPrice - 10;
+console.log(discountPrice);
