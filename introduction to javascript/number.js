4029b34d7dc4c4dfb003ebbfadb8c83a156073d4
mdalaminariya <king0878753@gmail.com>
2024-03-12 18:45:48 +0600

    number

---
+++ introduction to javascript/number.js
@@ -0,0 +1,16 @@
+var orangePrice = 25;
+var chocolate = 0.5;
+var applePrice =parseFloat('20.5') ; //eitar maddome integer e convert korbe sokol string jatio number ke.but character ke convert korte parbe na.
+
+console.log(orangePrice);
+console.log(applePrice)
+console.log(orangePrice + applePrice);
+
+
+var first = 0.1;
+var second = 0.2;
+var three = first + second;
+console.log(three.toFixed(2)); //tofixed er maddome dot er pore koyta ghor porjonto songkhathakbe oita bujay.
+
+const num = 50/0; //const er maddome infinity songkkha dekhay.
+console.log(num);
