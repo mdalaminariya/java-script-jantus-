86dca5d729fcf546032c0ac9e4c427a1468cf4ce
mdalaminariya <king0878753@gmail.com>
2024-03-13 22:49:06 +0600

    update if

---
+++ js.conditionals/if.js
@@ -0,0 +1,46 @@
+/**
+ *Tui jodi amake khawas,
+ *      tahole ami asbo..
+ * If you buy food for me,
+ *      then i arrive/come..
+ * 
+ * ------------------
+ * 
+ * If biriyani price less then 350tk
+ *              then i buy it.
+ * 
+ * 
+ * ---------structure---------
+ * 
+ * jodi condition sotti hoy tahole vitore kaj cholbe..
+ * if (condition){
+ */
+ if(13 < 10){
+    console.log('its true')
+ }
+
+
+//  var biriyaniPrice = 250;
+//  if(biriyaniPrice < 300){
+//     console.log('give me one plate biriyani.')
+//  }
+
+ /**
+  * if (condition){
+  *     execute code if above is true
+  * }
+  * 
+  * else{
+  *     execute code if condition false/.
+  * }
+  */
+
+
+ var weight = 20;
+
+ if (weight < 20){
+    console.log('I will carry it by my-self')
+ }
+ else{
+    console.log('I will rent a Rickshaw')
+ }
