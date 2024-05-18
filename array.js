621a44c450f595def3497e427cfa0c0a553f73b0
Alamin Arafat <155258300+mdalaminariya@users.noreply.github.com>
2024-05-18 15:10:24 +0600

    Add files via upload
    array

---
+++ array.js
@@ -0,0 +1,17 @@
+
+ // jodi amara kono kisur talika korte jai tokhon..
+ // number0 = 12
+ // number1 = 25
+ // number2 = 50
+ // number3 = 100
+ // ei bhabe liokhe thaki.
+ // abar const number = 2550100 eita ke amra just number hisebei bujte parbo ta chara kisui bujte parbo na..
+ // first braket () , second braket {} , third braket []..
+ //  array diclure er somoy 3rd braket tai sobai use kore thake..like
+ // array er maje man lekhar somoy coma (,) must dite hobe
+ // array er modde number , string(name,address etc),boolean..
+
+
+         const numbers = [12, 25, 50, 100]
+         const frind = ["abul", "kabul", "bulbul", "indhur" ]
+         const mixedArray = [true, false, false, true]
